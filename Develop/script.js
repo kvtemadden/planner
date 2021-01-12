ecd98d3c86efd573fbbb9fa034e211cc9e5c48a3
kate <katy.madden99@gmail.com>
2021-01-12 20:44:56 +0000

    Class changes depending on current time

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,6 @@
 jQuery(document).ready(function ($) {
-    var currentTime;
+    var now = moment();
+    console.log(now.format("HH"));
     var currentDay = moment().format("dddd MMMM Do"); //sets current date time format
     const startOfDay = moment().startOf('day');
     console.log(startOfDay);
@@ -7,7 +8,7 @@ jQuery(document).ready(function ($) {
     $(dayEl).text(currentDay); //sets text in element to current date
 
     // var hours = ["9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00"];
-    var hours = [9, 10, 11];
+    var hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
 
     for (var i = 0; i < hours.length; i++) { //for every hour listed in hours...
@@ -18,14 +19,26 @@ jQuery(document).ready(function ($) {
         var saveButton = $("<button>");
         var icon = $("<i>");
         debugger;
-        var hour = moment().set("hour", hours[i]).set("minute", 0).set("seconds", 0).format("hA");
+        var hour = startOfDay.add((hours[i]), "hour").set(0, "minutes").set(0, "seconds");
+        var hourFormatted = hour.format("hA");
         console.log(hour);
         $(container).append((block).addClass("row time-block")); // add a div to container and apply classes
-        $(block).append((timeText).text(hour).addClass("hour col-2")); // add a p tag to div with set text per hour and classes
+        $(block).append((timeText).text(hourFormatted).addClass("hour col-2")); // add a p tag to div with set text per hour and classes
         $(block).append((inputArea).addClass("description col-8")); // add form field/text area to div and apply classes
         $(block).append((saveButton).addClass("saveBtn col-2")); // add button to div and apply classes
         $(saveButton).append((icon).addClass("far fa-save")); // add save icon to button
+
+        if (moment(hour).isAfter(now) == true) {
+            $(inputArea).addClass("future");
+        }
+        else if (moment(now).isAfter(hour) == true) {
+            $(inputArea).addClass("past");
+        }
+        else if (moment(now).isSame(hour) == true) {
+            $(inputArea).addClass("present");
+        }
         
+        hour = startOfDay.subtract((hours[i]), "hour").set(0, "minutes").set(0, "seconds");
     }
 
 
