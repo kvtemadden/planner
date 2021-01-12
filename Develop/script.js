74721596f7eb01bffa5b32b5e285b49ea4963c7d
kate <katy.madden99@gmail.com>
2021-01-12 21:39:32 +0000

    Fixed the present class application

--- Develop/script.js
+++ Develop/script.js
@@ -2,13 +2,13 @@ jQuery(document).ready(function ($) {
     var now = moment();
     console.log(now.format("HH"));
     var currentDay = moment().format("dddd MMMM Do"); //sets current date time format
-    const startOfDay = moment().startOf('day');
+    var startOfDay = moment().startOf('day');
     console.log(startOfDay);
     var dayEl = $("#currentDay"); //getting element to store current date text in
     $(dayEl).text(currentDay); //sets text in element to current date
 
     // var hours = ["9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00"];
-    var hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+    var hours = [9, 10, 11, 12, 13, 14, 15, 16, 17, 21, 22];
 
 
     for (var i = 0; i < hours.length; i++) { //for every hour listed in hours...
@@ -27,15 +27,16 @@ jQuery(document).ready(function ($) {
         $(block).append((inputArea).addClass("description col-8")); // add form field/text area to div and apply classes
         $(block).append((saveButton).addClass("saveBtn col-2")); // add button to div and apply classes
         $(saveButton).append((icon).addClass("far fa-save")); // add save icon to button
-
-        if (moment(hour).isAfter(now) == true) {
+        var nextHour = moment().startOf('day').add((hours[i]), "hour").set(0, "minutes").set(0, "seconds");
+        nextHour.add(1, "hour");
+        if (now.isBetween(hour, nextHour) == true) {
+            $(inputArea).addClass("present"); 
+        }
+        else if (moment(hour).isAfter(now) == true) {
             $(inputArea).addClass("future");
         }
         else if (moment(now).isAfter(hour) == true) {
             $(inputArea).addClass("past");
-        }
-        else if (moment(now).isSame(hour, "hour") == true) {
-            $(inputArea).addClass("present");
         }
         
         hour = startOfDay.subtract((hours[i]), "hour").set(0, "minutes").set(0, "seconds");
